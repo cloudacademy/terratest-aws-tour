03d98178ac5f3c10153592aad1b580f360b563d3
Jun <jun.fritz@gmail.com>
2024-01-10 12:39:46 -1000

    starting point.

---
+++ examples/webserver/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "webserver" {
+    source = "../../"
+
+    servername = var.servername
+    size = "t2.micro"
+}
